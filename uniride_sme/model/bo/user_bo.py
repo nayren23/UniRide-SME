948cfe7c4bdb7bf0cc7c5e0dae2bc1158290303b
TheFanaticV2 <chefyaya93@outlook.com>
2024-01-08 10:34:04 +0100

    patch : user bo clean

--- uniride_sme/model/bo/user_bo.py
+++ uniride_sme/model/bo/user_bo.py
@@ -23,19 +23,4 @@ class UserBO:
     home_address_id: str = None
     work_address_id: str = None
     r_id: int= None
-    u_id: int = None
-    u_login: str = None
-    u_firstname: str = None
-    u_lastname: str = None
-    u_student_email: str = None
-    u_password: str = None
-    u_gender: str = None
-    u_phone_number: str = None
-    u_description: str = None
-    u_profile_picture: str = None
-    u_timestamp_creation: datetime = None
-    u_timestamp_modification: datetime = None
-    u_email_verified: str = None
-    u_status: str = None
-    u_home_address_id: str = None
-    u_work_address_id: str = None
+    
